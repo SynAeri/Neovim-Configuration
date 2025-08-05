efef2eeaae42db177bff08fb3d7d171793e2fc72
SynAeri <Jordan.Maquiran@outlook.com>
2025-08-06 08:15:18 +1000

    fixed lua of notetaking plugin

--- neovimConfig/config/lua/plugins/nvim-neorg.lua
+++ neovimConfig/config/lua/plugins/nvim-neorg.lua
@@ -1,8 +1,14 @@
-use {
-  "nvim-neorg/neorg",
-  rocks = { "lua-utils.nvim", "nvim-nio", "nui.nvim", "plenary.nvim", "pathlib.nvim", "nvim-treesitter-legacy-api" },
-  tag = "*", -- Pin Neorg to the latest stable release
-  config = function()
-      require("neorg").setup()
-  end,
+require("neorg").setup {
+  -- Add your neorg configuration here
+  load = {
+    ["core.defaults"] = {}, -- Loads default behaviour
+    ["core.concealer"] = {}, -- Adds pretty icons to your documents
+    ["core.dirman"] = { -- Manages Neorg workspaces
+      config = {
+        workspaces = {
+          notes = "~/notes",
+        },
+      },
+    },
+  },
 }
