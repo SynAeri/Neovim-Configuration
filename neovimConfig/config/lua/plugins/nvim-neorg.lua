b42499444fb17fd95d7d3b08d9f0b1d3a683efa8
SynAeri <Jordan.Maquiran@outlook.com>
2025-08-06 08:46:17 +1000

    removed some debugging stuff

--- neovimConfig/config/lua/plugins/nvim-neorg.lua
+++ neovimConfig/config/lua/plugins/nvim-neorg.lua
@@ -4,6 +4,7 @@ require("neorg").setup {
     ["core.defaults"] = {}, -- Loads default behaviour
     ["core.concealer"] = {}, -- Adds pretty icons to your documents
     ["core.dirman"] = { -- Manages Neorg workspaces
+    ["core.export.markdown"] = {},
       config = {
         workspaces = {
           notes = "~/notes",
