8e144b3fe9c025832765189a48586bc1955320e1
SynAeri <Jordan.Maquiran@outlook.com>
2025-08-11 16:58:36 +1000

    changed markdown configs

--- neovimConfig/config/lua/plugins/nvim-obsidian.lua
+++ neovimConfig/config/lua/plugins/nvim-obsidian.lua
@@ -1,4 +1,6 @@
 -- config/lua/plugins/nvim-obsidian.lua
+-- Pure obsidian.nvim functionality configuration
+
 require("obsidian").setup({
   -- Required: specify your vault(s)
   workspaces = {
@@ -6,11 +8,12 @@ require("obsidian").setup({
       name = "personal",
       path = vim.fn.expand("~/Documents/personalStuff"),
     },
-
     {
-      name = "work",
+      name = "work", 
       path = vim.fn.expand("~/Documents/studyStuff"),
     },
+  },
+
   -- Optional: log level
   log_level = vim.log.levels.INFO,
 
@@ -19,7 +22,7 @@ require("obsidian").setup({
     folder = "dailies",
     date_format = "%Y-%m-%d",
     alias_format = "%B %-d, %Y",
-    template = nil,  -- Use a template file if you have one
+    template = nil,
   },
 
   -- Completion settings
@@ -28,40 +31,13 @@ require("obsidian").setup({
     min_chars = 2,
   },
 
-  -- Mappings (set to false to disable, or remap)
-  mappings = {
-    -- "Obsidian follow"
-    ["gf"] = {
-      action = function()
-        return require("obsidian").util.gf_passthrough()
-      end,
-      opts = { noremap = false, expr = true, buffer = true },
-    },
-    -- Toggle checkbox
-    ["<leader>ch"] = {
-      action = function()
-        return require("obsidian").util.toggle_checkbox()
-      end,
-      opts = { buffer = true },
-    },
-    -- Smart action (follow link, toggle checkbox, etc.)
-    ["<cr>"] = {
-      action = function()
-        return require("obsidian").util.smart_action()
-      end,
-      opts = { buffer = true, expr = true },
-    },
-  },
-
   -- New note options
   new_notes_location = "notes_subdir",
   note_id_func = function(title)
-    -- Create note IDs from title, stripping spaces and special chars
     local suffix = ""
     if title ~= nil then
       suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
     else
-      -- If title is nil, use timestamp
       suffix = tostring(os.time())
     end
     return suffix
@@ -80,7 +56,7 @@ require("obsidian").setup({
 
   -- Templates
   templates = {
-    subdir = "templates",
+    folder = "templates", -- Updated from 'subdir' to 'folder'
     date_format = "%Y-%m-%d",
     time_format = "%H:%M",
     substitutions = {},
@@ -95,38 +71,43 @@ require("obsidian").setup({
     end,
   },
 
-  -- Picker configuration (works with telescope, fzf-lua, etc.)
+  -- Picker configuration
   picker = {
-    name = "telescope.nvim",  -- Change to your preferred picker
-    mappings = {
+    name = "telescope.nvim",
+    note_mappings = {
       new = "<C-x>",
       insert_link = "<C-l>",
     },
+    tag_mappings = {
+      tag_note = "<C-x>",
+      insert_tag = "<C-l>",
+    },
   },
 
-  -- UI options - DISABLE to avoid conflict with render-markdown
+  -- UI options - DISABLE to avoid conflict with markview
   ui = {
-    enable = false,  -- Disable obsidian UI to use render-markdown instead
+    enable = false, -- Using markview instead
   },
 
   -- Follow URL with system browser
   follow_url_func = function(url)
-    vim.fn.jobstart({"xdg-open", url})  -- Linux
-    -- vim.fn.jobstart({"open", url})     -- macOS
-    -- vim.cmd(':silent exec "!start ' .. url .. '"') -- Windows
+    vim.fn.jobstart({"xdg-open", url}) -- Linux
   end,
 
-  -- Use advanced URI for Obsidian app (updated syntax)
+  -- Advanced URI options (updated syntax)
   open = {
     app_name = "obsidian",
     use_advanced_uri = true,
   },
 
-  -- Enable legacy commands to avoid deprecation warning (set to false when ready to migrate)
-  legacy_commands = true,
+  -- Use new command syntax (no legacy commands)
+  legacy_commands = false, -- Changed to false
+
+  -- Disable deprecated mappings
+  mappings = {}, -- Empty table instead of deprecated mappings
 
   -- Finder options
-  finder = "telescope.nvim",  -- or "fzf-lua"
+  finder = "telescope.nvim",
   finder_mappings = {
     new = "<C-x>",
   },
@@ -136,72 +117,29 @@ require("obsidian").setup({
   
   -- YAML frontmatter options
   yaml_parser = "native",
-})
+}) -- <- Make sure this closing brace is here!
 
--- Setup render-markdown for better Obsidian experience
-require('render-markdown').setup({
-  enabled = true,
-  render_modes = { 'n', 'c', 't' },
-  preset = 'obsidian',  -- Use obsidian preset for callouts
-  file_types = { 'markdown' },
-  
-  -- LaTeX math rendering with maximum concealment
-  latex = {
-    enabled = true,
-    converter = 'latex2text',
-    highlight = 'RenderMarkdownMath',
-    position = 'above',  -- Show rendered math above
-    top_pad = 0,
-    bottom_pad = 0,
-  },
-  
-  -- Anti-conceal settings - hide as much as possible in normal mode
-  anti_conceal = {
-    enabled = true,
-    above = 0,  -- Don't show raw markdown above cursor
-    below = 0,  -- Don't show raw markdown below cursor
-    -- Remove most ignores to allow maximum concealment
-    ignore = {
-      -- Only keep essential elements that shouldn't be concealed
-      sign = true,
-    },
-  },
-  
-  -- Window options for better concealment
-  win_options = {
-    conceallevel = {
-      default = vim.o.conceallevel,
-      rendered = 3,  -- Maximum concealment in normal mode
-    },
-    concealcursor = {
-      default = vim.o.concealcursor,
-      rendered = '',  -- Show concealed text in all modes when cursor on line
-    },
-  },
-  
-  -- Obsidian-style checkboxes
-  checkbox = {
-    enabled = true,
-    unchecked = { icon = '󰄱 ', highlight = 'ObsidianTodo' },
-    checked = { icon = '󰱒 ', highlight = 'ObsidianDone' },
-    custom = {
-      todo = { raw = '[-]', rendered = '󰥔 ', highlight = 'ObsidianRightArrow' },
-    },
-  },
-  
-  -- Enhanced callouts for Obsidian
-  callout = {
-    note = { raw = '[!NOTE]', rendered = '󰋽 Note', highlight = 'ObsidianRefText' },
-    tip = { raw = '[!TIP]', rendered = '󰌶 Tip', highlight = 'ObsidianDone' },
-    important = { raw = '[!IMPORTANT]', rendered = '󰅾 Important', highlight = 'ObsidianTag' },
-    warning = { raw = '[!WARNING]', rendered = '󰀪 Warning', highlight = 'ObsidianRightArrow' },
-  },
-  
-  -- Better heading rendering
-  heading = {
-    enabled = true,
-    position = 'overlay',
-    icons = { '󰲡 ', '󰲣 ', '󰲥 ', '󰲧 ', '󰲩 ', '󰲫 ' },
-    width = 'full',
-  },
-})
+-- Custom keymaps for Obsidian commands (new command syntax)
+local function map(mode, lhs, rhs, opts)
+  opts = opts or {}
+  opts.silent = opts.silent ~= false
+  vim.keymap.set(mode, lhs, rhs, opts)
+end
+
+-- Manual keybindings for obsidian functionality (since mappings are deprecated)
+-- These replace the old mappings config
+map("n", "gf", function()
+  if require("obsidian").util.cursor_on_markdown_link() then
+    return "<cmd>Obsidian follow_link<cr>"
+  else
+    return "gf"
+  end
+end, { noremap = false, expr = true })
+
+map("n", "<cr>", function()
+  return require("obsidian").util.smart_action()
+end, { buffer = true, expr = true })
+
+map("n", "<leader>ch", function()
+  return require("obsidian").util.toggle_checkbox()
+end, { buffer = true })
