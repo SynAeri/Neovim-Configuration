3a74a8ae8a6ca45349319ac8da2566a9d57ecfad
SynAeri <Jordan.Maquiran@outlook.com>
2025-10-19 18:07:37 +1100

    fixed stuff for my wifi thingy

--- rtw8852cu.nix
+++ rtw8852cu.nix
@@ -1,8 +1,8 @@
-{ lib, stdenv, fetchFromGitHub, kernel, bc, nukeReferences }:
+{ lib, stdenv, fetchFromGitHub, kernel }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation {
   pname = "rtw8852cu";
-  version = "unstable-2024-12-08";
+  version = "2024-12-08";
 
   src = fetchFromGitHub {
     owner = "morrownr";
@@ -11,28 +11,24 @@ stdenv.mkDerivation rec {
     sha256 = "sha256-yp5e2ijkqKji+dJ+XPMJJPhkjh5NYUiFEncQc4HdNEA=";
   };
 
-  nativeBuildInputs = [ bc nukeReferences ] ++ kernel.moduleBuildDependencies;
-
-  hardeningDisable = [ "pic" "format" ];
-
-  makeFlags = kernel.makeFlags ++ [
-    "KSRC=${kernel.dev}/lib/modules/${kernel.modDirVersion}/build"
-    "KVER=${kernel.modDirVersion}"
-    "MODDESTDIR=$(out)/lib/modules/${kernel.modDirVersion}/kernel/drivers/net/wireless"
-  ];
-
-  preInstall = ''
-    mkdir -p $out/lib/modules/${kernel.modDirVersion}/kernel/drivers/net/wireless
+  nativeBuildInputs = kernel.moduleBuildDependencies;
+  
+  # The device ID is already in the morrownr repo (added Dec 27, 2024)
+  # Check supported-device-IDs file - 35bc:0102 should be there
+  
+  buildPhase = ''
+    make -C ${kernel.dev}/lib/modules/${kernel.modDirVersion}/build \
+      M=$(pwd) \
+      modules
   '';
 
-  installTargets = [ "install" ];
+  installPhase = ''
+    install -D -m644 8852cu.ko $out/lib/modules/${kernel.modDirVersion}/kernel/drivers/net/wireless/8852cu.ko
+  '';
 
-  enableParallelBuilding = true;
-
-  meta = with lib; {
-    description = "Realtek RTL8852CU/RTL8832CU USB WiFi driver";
-    homepage = "https://github.com/morrownr/rtl8852cu-20240510";
-    license = licenses.gpl2Only;
-    platforms = platforms.linux;
+  meta = {
+    description = "Realtek RTL8852CU USB WiFi driver";
+    license = lib.licenses.gpl2Only;
+    platforms = lib.platforms.linux;
   };
 }
